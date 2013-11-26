edc6e6f14ddcd688753eccc16d2cd2e5948875c8
tmptrash <tmptrash@mail.ru>
2013-11-26 12:00:26 +0200

    + Added onXXX() method in base.Controller and for App.base.App class

--- js/base/App.js
+++ js/base/App.js
@@ -6,6 +6,11 @@
  * only once (see example below). Remember, that you shouldn't store any reference
  * to this instance anywhere.
  *
+ * Events:
+ *     debug      Fires if debug message has generated.
+ *     beforerun  Fires before run() method will be called
+ *     run        Fires after run logic is finished
+ *
  * Usage:
  *     N13.init({appRoot: ['App', 'js']});  // Binds App namespace to js folder
  *     N13.create('App.Application');       // We don't need to save an app reference
@@ -17,10 +22,38 @@ N13.define('App.base.App', {
 
     /**
      * @interface
+     * Calls before main run logic. You may stop running by returning false in child class
+     * @returns {Boolean|undefined}
+     */
+    onBeforeRun: N13.emptyFn,
+    /**
+     * @interface
+     * Calls after main run logic. Is used for post run actions.
+     */
+    onAfterRun: N13.emptyFn,
+    /**
+     * @interface
+     * Calls after onBeforeRun() and before onAfterRun() methods. May be overridden in child classes.
+     * Is used for main (central) run logic.
+     */
+    onRun: N13.emptyFn,
+
+
+    /**
      * This is where an application starts and our html document is loaded and ready. You should
      * override this method in your child class.
+     * @returns {Boolean|undefined}
      */
-    run: N13.emptyFn,
+    run: function () {
+        this.trigger('beforerun');
+        if (this.onBeforeRun() === false) {
+            this.trigger('debug', 'Running of the application "' + this.className + '" was stopped, because onBeforeRun() method has returned false');
+            return false;
+        }
+        this.onRun();
+        this.onAfterRun();
+        this.trigger('run');
+    },
 
     /**
      * @constructor
